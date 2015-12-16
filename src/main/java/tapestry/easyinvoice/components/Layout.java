be90610d50f7918f099de6c71d6a6fd12af89ce9
dejan-ivanovic-2568 <divanovic@deform.rs>
2015-12-16 18:23:45 +0100

    Initial commit

---
+++ src/main/java/tapestry/easyinvoice/components/Layout.java
@@ -0,0 +1,22 @@
+package tapestry.easyinvoice.components;
+
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+
+/**
+ * 
+ * Dejan Ivanovic divanovic3d@gmail.com
+ */
+@Import(stylesheet = {
+    "context:css/normalize.css",
+    "context:css/bootstrap-custom.css",
+    "context:css/main.css"})
+public class Layout {
+
+    @Property
+    @Parameter(defaultPrefix = BindingConstants.LITERAL)
+    private String title;
+
+}
---
+++ src/main/java/tapestry/easyinvoice/pages/Index.java
@@ -0,0 +1,11 @@
+package tapestry.easyinvoice.pages;
+
+/**
+ * Start page of application EasyInvoice.
+ */
+public class Index {
+    
+    public Index() {
+    }
+    
+}
