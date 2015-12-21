b7d76c079d5b95f7afd7175b5b29a43bebf98170
dejan-ivanovic-2568 <divanovic@deform.rs>
2015-12-21 16:59:20 +0100

    db-update

--- easyinvoice_db.sql
+++ easyinvoice_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 21, 2015 at 10:09 AM
+-- Generation Time: Dec 21, 2015 at 04:58 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.28
 
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `client` (
   `clientId` int(11) NOT NULL,
-  `clientCompany` varchar(255) DEFAULT NULL,
-  `clientContact` varchar(32) NOT NULL,
   `clientAmount` double DEFAULT NULL,
+  `clientCompany` varchar(255) DEFAULT NULL,
+  `clientContact` varchar(255) DEFAULT NULL,
   `clientEmail` varchar(255) DEFAULT NULL,
   `clientIndustry` varchar(255) DEFAULT NULL,
   `clientPhone` varchar(255) DEFAULT NULL,
@@ -41,9 +41,9 @@ CREATE TABLE IF NOT EXISTS `client` (
 -- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`clientId`, `clientCompany`, `clientContact`, `clientAmount`, `clientEmail`, `clientIndustry`, `clientPhone`, `clientWebsite`) VALUES
-(10, 'clientCompany1', 'clientContact1', 0, 'clientEmail', 'clientIndustry1', 'clientPhone', 'clientWebsite'),
-(11, 'clientCompany2', 'clientContact2', 0, 'clientEmail2', 'clientIndustry2', 'clientPhone2', 'clientWebsite2');
+INSERT INTO `client` (`clientId`, `clientAmount`, `clientCompany`, `clientContact`, `clientEmail`, `clientIndustry`, `clientPhone`, `clientWebsite`) VALUES
+(10, 0, 'clientCompany1', 'clientContact1', 'clientEmail', 'clientIndustry1', 'clientPhone', 'clientWebsite'),
+(11, 0, 'Client2', 'Client2Contact', 'client2@email.com', 'Industry2', 'Client2Phone', 'client2Website');
 
 -- --------------------------------------------------------
 
@@ -53,23 +53,25 @@ INSERT INTO `client` (`clientId`, `clientCompany`, `clientContact`, `clientAmoun
 
 CREATE TABLE IF NOT EXISTS `invoice` (
   `invoiceId` int(11) NOT NULL,
-  `invoiceNumber` varchar(16) NOT NULL,
   `invoiceAmount` double DEFAULT NULL,
   `invoiceCurrency` varchar(255) DEFAULT NULL,
   `invoiceDescription` varchar(255) DEFAULT NULL,
   `invoiceDueDate` date DEFAULT NULL,
   `invoiceIssueDate` date DEFAULT NULL,
+  `invoiceNumber` varchar(255) DEFAULT NULL,
   `invoiceStatus` varchar(255) DEFAULT NULL,
   `invoiceClientId` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `invoice`
 --
 
-INSERT INTO `invoice` (`invoiceId`, `invoiceNumber`, `invoiceAmount`, `invoiceCurrency`, `invoiceDescription`, `invoiceDueDate`, `invoiceIssueDate`, `invoiceStatus`, `invoiceClientId`) VALUES
-(7, 'null', 400, 'USD', 'description1', '2015-12-18', '2015-12-18', 'Open', 11),
-(8, 'null', 200, 'EUR', 'description2', '2015-12-18', '2015-12-18', 'Overdue', 11);
+INSERT INTO `invoice` (`invoiceId`, `invoiceAmount`, `invoiceCurrency`, `invoiceDescription`, `invoiceDueDate`, `invoiceIssueDate`, `invoiceNumber`, `invoiceStatus`, `invoiceClientId`) VALUES
+(13, 400, 'USD', 'description1', '2015-12-21', '2015-12-21', 'null', 'Open', 10),
+(14, 0, 'USD', NULL, '2015-12-24', '2015-12-21', '2015-1221', NULL, 10),
+(15, 0, 'USD', NULL, '2015-12-19', '2015-12-21', '2015-1221', NULL, 10),
+(16, 0, 'GBP', NULL, '2015-12-07', '2015-12-01', '2015-1221', NULL, 11);
 
 -- --------------------------------------------------------
 
@@ -80,19 +82,17 @@ INSERT INTO `invoice` (`invoiceId`, `invoiceNumber`, `invoiceAmount`, `invoiceCu
 CREATE TABLE IF NOT EXISTS `member` (
   `memberId` int(11) NOT NULL,
   `memberName` varchar(255) DEFAULT NULL,
-  `memberUsername` varchar(255) DEFAULT NULL,
   `memberPassword` varchar(255) DEFAULT NULL,
-  `memberRole` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+  `memberRole` varchar(255) DEFAULT NULL,
+  `memberUsername` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`memberId`, `memberName`, `memberUsername`, `memberPassword`, `memberRole`) VALUES
-(1, 'Administrator', 'admin', 'admin', 'Administrator'),
-(8, 'user', 'user', 'user', 'User'),
-(14, 'Supervisor', 'sup', 'sup', 'User');
+INSERT INTO `member` (`memberId`, `memberName`, `memberPassword`, `memberRole`, `memberUsername`) VALUES
+(1, 'Administrator', 'admin', 'Administrator', 'admin');
 
 -- --------------------------------------------------------
 
@@ -126,9 +126,21 @@ INSERT INTO `registration` (`registrationId`, `registrationAddress`, `registrati
 
 CREATE TABLE IF NOT EXISTS `service` (
   `serviceId` int(11) NOT NULL,
-  `serviceDescription` varchar(32) NOT NULL,
-  `serviceAmount` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `serviceAmount` double DEFAULT NULL,
+  `serviceDescription` varchar(255) DEFAULT NULL,
+  `invoiceServiceId` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`serviceId`, `serviceAmount`, `serviceDescription`, `invoiceServiceId`) VALUES
+(5, 400, 'Service 1 description', 13),
+(6, 1000, 'Service 1-2 description', 13),
+(7, 30, 'dawdawd', 14),
+(8, 30, 'dwadwadawd', 15),
+(9, 30, 'fbdfgbedbf', 16);
 
 --
 -- Indexes for dumped tables
@@ -145,7 +157,7 @@ ALTER TABLE `client`
 --
 ALTER TABLE `invoice`
   ADD PRIMARY KEY (`invoiceId`),
-  ADD KEY `invoiceClient_FK` (`invoiceClientId`);
+  ADD KEY `FK_roj299l1vk25hjmfnbtktcad5` (`invoiceClientId`);
 
 --
 -- Indexes for table `member`
@@ -164,7 +176,7 @@ ALTER TABLE `registration`
 --
 ALTER TABLE `service`
   ADD PRIMARY KEY (`serviceId`),
-  ADD UNIQUE KEY `serviceId` (`serviceId`);
+  ADD KEY `FK_2eyaovgqiq0pre63prev6yeje` (`invoiceServiceId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -179,17 +191,17 @@ ALTER TABLE `client`
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `invoiceId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `invoiceId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `memberId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
+  MODIFY `memberId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
-  MODIFY `serviceId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `serviceId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
@@ -198,7 +210,13 @@ ALTER TABLE `service`
 -- Constraints for table `invoice`
 --
 ALTER TABLE `invoice`
-  ADD CONSTRAINT `invoiceClient_FK` FOREIGN KEY (`invoiceClientId`) REFERENCES `client` (`clientId`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_roj299l1vk25hjmfnbtktcad5` FOREIGN KEY (`invoiceClientId`) REFERENCES `client` (`clientId`);
+
+--
+-- Constraints for table `service`
+--
+ALTER TABLE `service`
+  ADD CONSTRAINT `FK_2eyaovgqiq0pre63prev6yeje` FOREIGN KEY (`invoiceServiceId`) REFERENCES `invoice` (`invoiceId`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
